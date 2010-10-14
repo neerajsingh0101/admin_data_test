7b98d07751e45fc6ed4be0c1df3ac736d1cd6add
Neeraj Singh <neerajdotname@gmail.com>
2010-10-13 20:30:52 -0400

    refactor code move to case when

--- features/step_definitions/app_steps.rb
+++ features/step_definitions/app_steps.rb
@@ -1,9 +1,10 @@
 When /^I visit (.*) page$/ do |target_page|
-  if target_page == 'admin_data'
+  case target_page
+  when 'admin_data'
     visit '/admin_data'
-  elsif target_page == 'quick_search'
+  when 'quick_search'
     visit '/admin_data/quick_search/user'
-  elsif target_page == 'user show'
+  when 'user show'
     visit "/admin_data/klass/user/#{User.last.id}"
   end
 end
