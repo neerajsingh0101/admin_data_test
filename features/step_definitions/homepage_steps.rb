c0c51fd8486907432bcfac076f2df15e21406b9e
Neeraj Singh <neerajdotname@gmail.com>
2010-11-05 00:22:10 -0400

    refactoring

--- features/step_definitions/homepage_steps.rb
+++ features/step_definitions/homepage_steps.rb
@@ -4,41 +4,32 @@ Then /^page should have link with text "(.*)" within "(.*)" and url "(.*)"$/ do 
   page.should have_xpath( "//div[@id='#{id}']//a[@href='#{url}']", :text => text )
 end
 
+def handy_select(css_selector, select_options)
+  page.should have_xpath("//select[@class='#{css_selector}']")
+  selects = page.find(:xpath, "//select[@class='#{css_selector}']")
 
-def foo(selects, index, text)
-  selects.find(:xpath, "./option[#{index}]", :text => text)
-end
-
-def foo2(selects, index, text)
-  selects.find(:xpath, "./option[#{index}]").value.should match Regexp.new(text)
+  select_options.each do |h|
+    selects.find(:xpath, "./option[#{h[:position]}]", :text => h[:text])
+    selects.find(:xpath, "./option[#{h[:position]}]").value.should match Regexp.new(h[:value])
+  end
 end
 
 #TODO try to convert it into a utility so that name and url could be passed in the feature
 Then /^I should see page with proper dropdown values$/ do
-  page.should have_xpath("//select[@class='drop_down_value_klass']")
-  selects =  page.find(:xpath, "//select[@class='drop_down_value_klass']")
-
-  foo(selects, 2, 'phone_number')
-  foo(selects, 3, 'user')
-  foo(selects, 4, 'website')
-
+  css_selector = 'drop_down_value_klass'
   url = '/admin_data/quick_search/'
-  foo2(selects, 2, url+'phone_number')
-  foo2(selects, 3, url+'user')
-  foo2(selects, 4, url+'website')
+
+  select_options = []
+  select_options << {:position => 2, :text => 'phone_number', :value => "#{url}phone_number"}
+  select_options << {:position => 3, :text => 'user', :value => "#{url}user"}
+  select_options << {:position => 4, :text => 'website', :value => "#{url}website"}
+
+  handy_select(css_selector, select_options)
 end
 
 
 
 =begin
-
-a = []
-a << Array.wrap({:text => 'phone_number', :value => "#{url}phone_number"})
-a << Array.wrap({:text => 'user', :value => "#{url}user"})
-a << Array.wrap({:text => 'website', :value => "#{url}website"})
-
-page.handy_has_select?(css_selector, a)
-
 
 # css_selector  only class names and ids are currently suppported.
 # Usage:
