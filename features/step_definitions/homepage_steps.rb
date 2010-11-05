b85d7937bbcdaebc8573e5d6ceda6a437d39e6fb
Neeraj Singh <neerajdotname@gmail.com>
2010-11-04 23:48:58 -0400

    refactor

--- features/step_definitions/homepage_steps.rb
+++ features/step_definitions/homepage_steps.rb
@@ -4,21 +4,47 @@ Then /^page should have link with text "(.*)" within "(.*)" and url "(.*)"$/ do 
   page.should have_xpath( "//div[@id='#{id}']//a[@href='#{url}']", :text => text )
 end
 
+
+def foo(selects, index, text)
+  selects.find(:xpath, "./option[#{index}]", :text => text)
+end
+
+def foo2(selects, index, text)
+  selects.find(:xpath, "./option[#{index}]").value.should match Regexp.new(text)
+end
+
 #TODO try to convert it into a utility so that name and url could be passed in the feature
 Then /^I should see page with proper dropdown values$/ do
+  page.should have_xpath("//select[@class='drop_down_value_klass']")
   selects =  page.find(:xpath, "//select[@class='drop_down_value_klass']")
-  selects.should be_true
 
-  selects.find(:xpath, './option[2]', :text => 'phone_number')
-
-  selects.find(:xpath, './option[3]', :text => 'user')
-
-  selects.find(:xpath, './option[4]', :text => 'website')
+  foo(selects, 2, 'phone_number')
+  foo(selects, 3, 'user')
+  foo(selects, 4, 'website')
 
   url = '/admin_data/quick_search/'
-  selects.find(:xpath, './option[2]').value.should match Regexp.new(url+'phone_number')
-
-  selects.find(:xpath, './option[3]').value.should match Regexp.new(url+'user')
-  
-  selects.find(:xpath, './option[4]').value.should match Regexp.new(url+'website')
+  foo2(selects, 2, url+'phone_number')
+  foo2(selects, 3, url+'user')
+  foo2(selects, 4, url+'website')
 end
+
+
+
+=begin
+
+a = []
+a << Array.wrap({:text => 'phone_number', :value => "#{url}phone_number"})
+a << Array.wrap({:text => 'user', :value => "#{url}user"})
+a << Array.wrap({:text => 'website', :value => "#{url}website"})
+
+page.handy_has_select?(css_selector, a)
+
+
+# css_selector  only class names and ids are currently suppported.
+# Usage:
+#
+# page.handy_has_select?('.active_users', [[{:text => 'India', :value => 'IN'}], [{:text => 'America', :value => 'USA']])
+def handy_has_select?(css_selector, array)
+end
+     
+=end
