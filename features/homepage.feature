969e3370fd1e377ec60412e9348af44f73a3029d
Neeraj Singh <neerajdotname@gmail.com>
2010-11-05 11:47:14 -0400

    cukes

--- features/homepage.feature
+++ features/homepage.feature
@@ -13,10 +13,16 @@ Feature: homepage
     When I follow "admin_data"
     Then I should see "Select from the drop down menu above"
     Then I should see dropdown with css_selector ".drop_down_value_klass" with following options:
-      | text         | value                                 | position |
-      | phone_number | /admin_data/quick_search/phone_number | 2        |
-      | user         | /admin_data/quick_search/user         | 3        |
-      | website      | /admin_data/quick_search/website      | 4        |
+      | text         | value                                 | position | value_match_type |
+      | phone_number | /admin_data/quick_search/phone_number | 2        | regex |
+      | user         | /admin_data/quick_search/user         | 3        | regex |
+      | website      | /admin_data/quick_search/website      | 4        | regex |
+
+    Then I should see dropdown with css_selector "#drop_down_klasses" with following options:
+      | text         | value                                 | position | value_match_type |
+      | phone_number | /admin_data/quick_search/phone_number | 2        | regex            |
+      | user         | /admin_data/quick_search/user         | 3        | regex            |
+      | website      | /admin_data/quick_search/website      | 4        | regex            |
     When I select "user" from "drop_down_klasses"
     Then show me the page
     Then first id of table should be of "Trisha"
--- features/step_definitions/homepage_steps.rb
+++ features/step_definitions/homepage_steps.rb
@@ -4,29 +4,30 @@ Then /^page should have link with text "(.*)" within "(.*)" and url "(.*)"$/ do 
   page.should have_xpath( "//div[@id='#{id}']//a[@href='#{url}']", :text => text )
 end
 
-def handy_select(css_selector, select_options)
-  page.should have_xpath("//select[@class='#{css_selector}']")
-  selects = page.find(:xpath, "//select[@class='#{css_selector}']")
+def handy_has_select?(css_selector, select_options)
+  selector = css_selector[1..-1]
+  case css_selector[0,1]
+  when '.'
+    attribute = "@class='#{selector}'"
+  when '#'
+    attribute = "@id='#{selector}'"
+  else
+    raise 'only css_selector class or id is currently supported'
+  end
+
+  page.should have_xpath("//select[#{attribute}]")
+  selects = page.find(:xpath, "//select[#{attribute}]")
 
   select_options.each do |h|
     selects.find(:xpath, "./option[#{h[:position]}]", :text => h[:text])
-    selects.find(:xpath, "./option[#{h[:position]}]").value.should match Regexp.new(h[:value])
+    if h[:value_match_type] == 'regex'
+      selects.find(:xpath, "./option[#{h[:position]}]").value.should match Regexp.new(h[:value])
+    else
+      selects.find(:xpath, "./option[#{h[:position]}]", :text => h[:value])
+    end
   end
 end
 
-Then /^I should see dropdown with css_selector ".(.*)" with following options:$/ do |css_selector, table|
-  handy_select(css_selector, table.hashes)
+Then /^I should see dropdown with css_selector "(.*)" with following options:$/ do |css_selector, table|
+  handy_has_select?(css_selector, table.hashes)
 end
-
-
-
-=begin
-
-# css_selector  only class names and ids are currently suppported.
-# Usage:
-#
-# page.handy_has_select?('.active_users', [[{:text => 'India', :value => 'IN'}], [{:text => 'America', :value => 'USA']])
-def handy_has_select?(css_selector, array)
-end
-     
-=end
